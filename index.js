3fde7dde440559333fd5d2eeb09b43296c1a606a
anhulife <anhulife@gmail.com>
2015-07-23 14:14:54 +0800

    使用新的插件封装方式

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var autoprefixer = require('autoprefixer-core');
+var mutil = require('miaow-util');
 var postcss = require('postcss');
 
-module.exports = function (option, cb) {
+var pkg = require('./package.json');
+
+module.exports = mutil.plugin(pkg.name, pkg.version, function (option, cb) {
   postcss([autoprefixer]).process(this.contents).then(function (result) {
     this.contents = new Buffer(result.css);
     cb();
   }.bind(this));
-};
+});
