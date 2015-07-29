3da632f86e4996d559337c646a49306bc07892a1
anhulife <anhulife@gmail.com>
2015-07-29 18:38:15 +0800

    完善对空内容文件的处理

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var postcss = require('postcss');
 var pkg = require('./package.json');
 
 module.exports = mutil.plugin(pkg.name, pkg.version, function (option, cb) {
-  var contents = this.contents;
-  if (!contents.length) {
+  var contents = this.contents.toString();
+  if (!contents.trim()) {
     return cb();
   }
 
-  postcss([autoprefixer]).process(this.contents).then(function (result) {
+  postcss([autoprefixer]).process(contents).then(function (result) {
     this.contents = new Buffer(result.css);
     cb();
   }.bind(this));
